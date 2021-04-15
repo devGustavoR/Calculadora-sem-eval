8d457f88dc9e8c39525c9464a35d8279f496ae5f
Wuors <gustaano881@gmail.com>
2021-04-14 22:21:47 -0300

    Tudo pronto

--- scripts.js
+++ scripts.js
@@ -1,121 +1,39 @@
-class Calculadora{
-  constructor(input,output){
-    this.inputDisplay = input;
-    this.outputDisplay = output;
-    this.inputHistory = [];
-  }
-
-  clearAllHistory(){
-    this.inputHistory = [];
-    this.updateInputDisplay();
-    this.updateOutputDisplay("0");
-  }
-
-  backspace(){}
-
-  changePercentToDecimal(){}
-
-  insertNumber(value){
-    if(this.getLastInputType() === "number"){
-      this.appendToLastInput(value);  
-    }
-    else if(this.getLastInputType() === "operator" || this.getLastInputType() === null){
-      this.addNewInput(value,"number");
-    }
-  }
-
-  insertOperation(value){}
-
-  negativeNumber(){}
-
-  insertDecimalPoint(){}
-
-  generateResult(){}
-
-  //Helper Functions
-
-  getLastInputType(){
-    return (this.inputHistory.length === 0) ? null :this.inputHistory[this.inputHistory.length -1].type;
-  }
-
-  getLastInputValue(){
-    return (this.inputHistory.length === 0) ? null :this.inputHistory[this.inputHistory.length -1].value;
-  }
-
-  getAllInputValues(){
-    return this.inputHistory.map(entry =>entry.value);
-  }
-
-  getOutputValue(){
-    return this.outputDisplay.value.replace(/,/g,'');
-  }
-
-  addNewInput(value,type){
-    this.inputHistory.push({"type":type, "value":value.toString() });
-    this.updateInputDisplay();
-  }
-
-  updateInputDisplay(){
-    this.inputDisplay.value = this.getAllInputValues().join(" ");
-  }
-  updateOutputDisplay(value){
-    this.outputDisplay.value = Number(value).toLocaleString();
-  }
-
+function button(a)
+{	
+	document.getElementById("ans").value+=a;
 }
 
-const inputDisplay = document.querySelector("#history");
-const outputDisplay = document.querySelector("#result");
-
-const allClearButton = document.querySelector("[data-all-clear]");
-const backspaceButton = document.querySelector("[data-backspace]");
-const percentButton = document.querySelector("[data-percent]");
-const operationButtons = document.querySelector("[data-operator]");
-const numberButtons = document.querySelector("[data-number]");
-const negativeButton = document.querySelector("[data-negative]");
-const decimalButton = document.querySelector("[data-decimal]");
-const equalsButton = document.querySelector("[data-equals]");
-
-const c1 = new Calculadora(inputDisplay, outputDisplay);
-
-allClearButton.addEventListener("click",()=>{
-  c1.clearAllHistory
-});
-
-backspaceButton.addEventListener("click",()=>{
-  c1.backspace();
-});
-
-percentButton.addEventListener("click",()=>{
-  c1.changePercentToDecimal();
-});
-
-operationButtons.forEach(button=>{
-  button.addEventListener("click",(event)=>{
-    let{target} = event;
-    c1.insertOperation(target.dataset.operation);
-  })
-});
-
-numberButtons.forEach(button=>{
-  button.addEventListener("click",(event)=>{
-    let{target} = event;
-    c1.insertOperation(target.dataset.number);
-  })
-});
-
-negativeButton.addEventListener("click",()=>{
-  c1.negativeNumber();
-});
-
-backspaceButton.addEventListener("click",()=>{
-  c1.backspace();
-});
-
-percentButton.addEventListener("click",()=>{
-  c1.changePercentToDecimal();
-});
-
-equalsButton.addEventListener("click",()=>{
-  c1.generateResult();
-});
+function che(){
+		var res = [];
+		var answer;
+		
+		if(document.getElementById("ans").value.includes("+")){
+		res = document.getElementById("ans").value.split('+');
+		return parseInt(res[0]) + parseInt(res[1]);
+		}
+		
+		else if(document.getElementById("ans").value.includes("-")){
+		res = document.getElementById("ans").value.split('-');
+		return parseInt(res[0]) - parseInt(res[1]);
+		}
+		
+		else if(document.getElementById("ans").value.includes("/")){
+		res = document.getElementById("ans").value.split('/');
+		return parseInt(res[0]) / parseInt(res[1]);
+		}
+		
+		else{
+		res = document.getElementById("ans").value.split('*');
+		return answer= parseInt(res[0]) * parseInt(res[1]);
+		}
+}
+		
+function button1(){	
+var a = che()
+document.getElementById('ans').value = a; 
+}
+	
+function buttonclear()
+	{
+		document.getElementById('ans').value=" ";	
+	}
